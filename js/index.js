f8dce4ff60818dcf8682d71863b644594801b9bf
Vasyl Dumych <v.doomich@gmail.com>
2017-09-25 16:04:37 +0000

    Initial

--- js/index.js
+++ js/index.js
@@ -12,14 +12,12 @@ function solveQuadr(a, b, c) {
         const x1 = (-b + Math.sqrt(d)) / (2 * a);
         const x2 = (-b - Math.sqrt(d)) / (2 * a);
         return 'x1 = ' + x1 + ', x2 = ' + x2;
-    }
-    else if (d === 0) {
+    } else if (d === 0) {
         const x = (-b) / (2 * a);
-    }
-    else {
+    } else {
         return 'No roots';
     }
 }
-
+ 
 const result = solveQuadr(a, b, c);
 document.write(result);
