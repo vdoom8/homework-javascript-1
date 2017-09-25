7c6f66fb031a8c3cf0fe12a7228fbc4ede87a8b2
Vasyl Dumych <v.doomich@gmail.com>
2017-09-25 12:18:49 +0000

    Initial

---
+++ js/index.js
@@ -0,0 +1,23 @@
+const a = Number(prompt('Enter A'));
+const b = Number(prompt('Enter B'));
+const c = Number(prompt('Enter C'));
+
+function disc(a, b, c) {
+    return b * b - 4 * a * c;
+}
+
+function solveQuadr(a, b, c) {
+   const d = disc(a, b, c);
+   if (d > 0) {
+     const x1 = ( - b + Math.sqrt( d ) ) / ( 2 * a);
+     const x2 = ( - b - Math.sqrt( d ) ) / ( 2 * a);
+     return 'x1 = ' + x1 + ', x2 = ' + x2;
+   } else if (d === 0) {
+       
+   } else {
+       return 'No roots';
+   }
+}
+
+const result = solveQuadr(a,b,c);
+document.write(result);
